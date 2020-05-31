3f7947751d60063490c75ad302aca994d404d3d9
deovaliandro <mdv16h@student.unhas.ac.id>
2020-06-01 07:12:30 +0800

    1002 - Area lingkaran

---
+++ 1002.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+	float r, pi = 3.14159;
+	cin >> r;
+	
+	float a = pi * pow(r, 2);
+	printf("%0.4f\n", a);
+	
+	return 0;
+}
