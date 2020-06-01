417998e9c8bd1d53988e161a78426e7ce56e0bd2
deovaliandro <mdv16h@student.unhas.ac.id>
2020-06-02 01:33:21 +0800

    1014 - Consumption

---
+++ 1014.cpp
@@ -0,0 +1,14 @@
+// 1014 - Consumption
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    int x;
+    float y;
+    cin >> x; cin >> y;
+
+    printf("%0.3f km/l", float(x/y));
+
+	return 0;
+}
