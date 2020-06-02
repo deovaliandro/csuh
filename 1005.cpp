e1df735bddcbe5ae67381f71755c24b4f357699e
deovaliandro <mdv16h@student.unhas.ac.id>
2020-06-02 13:06:31 +0800

    1005 - Average 1 (with weight value)

--- 1005.cpp
+++ 1005.cpp
@@ -4,11 +4,9 @@ using namespace std;
 
 int main(){
     float a, b, grade;
-    cin >> a;
-    cin >> b;
+    cin >> a >> b;
 
     grade = ((a*3.5)+(b*7.5))/(3.5+7.5);
-
     printf("MEDIA = %0.5f\n", grade);
 
     return 0;
