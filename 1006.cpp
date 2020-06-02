4b845902fd211fa4f6c2b5bf02443f52bac6a6bb
deovaliandro <mdv16h@student.unhas.ac.id>
2020-06-02 13:07:26 +0800

    1006 - Average 2 (with 3 values)

--- 1006.cpp
+++ 1006.cpp
@@ -4,11 +4,10 @@ using namespace std;
 
 int main(){
     float a, b, c, media;
-    cin >> a; cin >> b; cin >> c;
-
+    cin >> a >> b >> c;
+    
     media = ((a*2)+(b*3)+(c*5))/10;
-
     printf("MEDIA = %0.1f\n", media);
-
+    
 	return 0;
 }
