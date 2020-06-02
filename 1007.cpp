c1fbb16b813123254a714de970c3e8fcb55db3a0
deovaliandro <mdv16h@student.unhas.ac.id>
2020-06-02 13:08:29 +0800

    1007 - Difference

--- 1007.cpp
+++ 1007.cpp
@@ -4,10 +4,9 @@ using namespace std;
 
 int main(){
     int a, b, c, d, diff;
-    cin >> a; cin >> b; cin >> c; cin >> d;
+    cin >> a >> b >> c >> d;
 
     diff = (a*b) - (c*d);
-
     printf("DIFERENCA = %d\n", diff);
 
 	return 0;
