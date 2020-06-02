aa0c35aff24618365da3afa7af662d4b771956e8
deovaliandro <mdv16h@student.unhas.ac.id>
2020-06-02 22:32:12 +0800

    1035 - Selection Test 1

--- 1035.py
+++ 1035.py
@@ -7,4 +7,4 @@ d = int(input())
 if (b>c) and (d>a) and (c+d)>(a+b) and c > 0 and d > 0 and a%2==0:
     print("Valores aceitos\n")
 else:
-    print("Valores nao aceitos")
+    print("Valores nao aceitos\n")
