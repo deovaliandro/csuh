6bb5b079f521f6ccd7b8cd69a88b69655a413075
deovaliandro <mdv16h@student.unhas.ac.id>
2020-06-02 13:10:09 +0800

    1009 - Salary with Bonus

--- 1009.cpp
+++ 1009.cpp
@@ -5,10 +5,9 @@ using namespace std;
 int main(){
     string name;
     float salary, total, fsalary;
-    cin >> name; cin >> salary; cin >> total;
+    cin >> name >> salary >> total;
     
     fsalary = salary + ((total*15)/100);
-
     printf("TOTAL = R$ %0.2f\n", fsalary);
 
 	return 0;
