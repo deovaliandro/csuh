e2abd1d538e6041ef32609e716454b93714bd4cc
deovaliandro <mdv16h@student.unhas.ac.id>
2020-06-02 22:31:29 +0800

    1036 - Bhaskara's Formula

--- 1036.py
+++ 1036.py
@@ -9,7 +9,7 @@ if a > 0.0 or b > 0.0 or c > 0.0 :
     aa = ((b*(-1)) + (math.sqrt(pow(b,2)-(4*a*c))))/(2*a)
     ab = ((b*(-1)) - (math.sqrt(pow(b,2)-(4*a*c))))/(2*a)
 
-    print("R1 = %0.5f" % aa)
-    print("R2 = %0.5f" % ab)
+    print("R1 = %0.5f\n" % aa)
+    print("R2 = %0.5f\n" % ab)
 else :
     print("Impossivel calcular")
